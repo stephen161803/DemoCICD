c637352dddebb28dbd0c2935125cb22ab3393148
Vo Nhat Thanh <vnt8587@gmail.com>
2026-02-25 10:16:08 +0700

    Initial ASP.NET Core app

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoCICD.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
